eff375452ba19d7585d6d75a0103112ee1f1ac00
Sparsh-R17 <sparsh170702@gmail.com>
2024-05-12 22:04:54 +0530

    Initial Commit

---
+++ app/src/main/java/com/project/friend_chat/services/FCMNotificationService.java
@@ -0,0 +1,6 @@
+package com.project.friend_chat.services;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+
+public class FCMNotificationService extends FirebaseMessagingService {
+}
